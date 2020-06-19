7c010bef1680566f2cf4acd57458b7387714ee42
thiagodias63 <thidias6003@gmail.com>
2020-06-19 14:13:58 -0300

    Removida linha de comentário

--- questao_2.php
+++ questao_2.php
@@ -1,5 +1,4 @@
 <?php
-// -------------------------------------------------------------------------------------
 // Função recursiva *
 // Crie uma função recursiva para descobrir o menor número inteiro divisível por 2, 3 e 10
 // ao mesmo tempo. Quando encontrá-lo, imprima-o na tela.
